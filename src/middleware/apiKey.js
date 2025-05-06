0086bde7a6614b72a6f2d694ea9c90ffb3efa983
Julianeves01 <julia.s.neves6@aluno.senai.br>
2025-05-06 08:44:49 -0300

    create apiKey.js

---
+++ src/middleware/apiKey.js
@@ -0,0 +1,10 @@
+const validApiKey = "JJavo2vIImk18VtDyoDm50pItAVO0Z";
+
+module.exports = (req, res, next) => {
+    const apiKey = req.header("x-api-key");
+    if (apiKey === validApiKey) {
+        next(); // Chave válida, prossiga
+    } else {
+        res.status(401).json({ error: "API Key inválida" }); // Chave inválida
+    }
+};
