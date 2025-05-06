bc04ce3d0032f8f2a576b9cbc9bcb06ffefb90ea
Julianeves01 <julia.s.neves6@aluno.senai.br>
2025-05-06 14:45:54 -0300

    finished

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ const app = express();
 const cors = require("cors");
 const autorRoutes = require("./src/routes/autorRoutes");
 const livroRoutes = require("./src/routes/livroRoutes");
-const reportRoutes = require("./src/routes/reportRoutes")
+const reportRoutes = require("./src/routes/reportRoutes");
 
 app.use(cors());
 app.use(express.json()); // Middleware para processar JSON
 
 // Rotas
+
 app.use("/api/autor", autorRoutes);
 app.use("/api/livros", livroRoutes);
-app.use("/api", reportRoutes); 
+app.use("/api", reportRoutes);
 app.use("/autores", autorRoutes);
 
-
-const PORT = 3000;
+// Inicialização do servidor
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🎀 Servidor rodando na porta ${PORT}`);
+    console.log(` 🎀 Servidor rodando na porta ${PORT}`);
 });
